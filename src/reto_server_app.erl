e4af5d27db2d2f2b1990c17066efbd585780be29
akxs14 <angelos.kapsimanis@madvertise.com>
2014-04-03 22:19:45 +0200

    Add rebar skeleton

---
+++ src/reto_server_app.erl
@@ -0,0 +1,16 @@
+-module(reto_server_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    reto_server_sup:start_link().
+
+stop(_State) ->
+    ok.
