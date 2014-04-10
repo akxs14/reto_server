39065b6c179fc5f02840ec75fdd8144c3605a825
akxs14 <angelos.kapsimanis@madvertise.com>
2014-04-10 20:21:15 +0200

    Added sample bid request in post.sh

--- apps/reto_server/openrtb2_bid_request_handler.erl
+++ apps/reto_server/openrtb2_bid_request_handler.erl
@@ -6,6 +6,10 @@
 -export([handle_bid_request/2]).
 -export([terminate/3]).
 
+%% ===================================================================
+%% Cowboy API
+%% ===================================================================
+
 init(_Transport, _Req, []) ->
     {upgrade, protocol, cowboy_rest}.
 
@@ -17,12 +21,6 @@ content_types_accepted(Req, State) ->
     {{<<"application">>,<<"json">>,[]}, handle_bid_request}
   ], Req, State}.
 
-handle_bid_request(Req, State) ->
-  {ok, Req2} = cowboy_req:reply(200, 
-    [{<<"content-type">>, <<"application/json">>}], 
-     jiffy:encode(<<"{\"rest\": \"Hello World!\"}">>), Req ),
-  {halt, Req2, State}.
-
 terminate(_Reason, _Req, _State) ->
   ok.
 
@@ -32,6 +30,12 @@ terminate(_Reason, _Req, _State) ->
 
 % prepare_bid_response(_SelectedAd) ->
 %   ok.
+
+handle_bid_request(Req, State) ->
+  {ok, Req2} = cowboy_req:reply(200, 
+    [{<<"content-type">>, <<"application/json">>}], 
+     jiffy:encode(<<"{\"rest\": \"Hello World!\"}">>), Req ),
+  {halt, Req2, State}.
 
 % handle_bid_request(Req, State) ->
   %BidReq = openrtb2_bid_request_parser:parse(Req),
